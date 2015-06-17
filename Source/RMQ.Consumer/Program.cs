28952ecf5578050b75d53f0962fa94e82a4dda9c
SergerGood <serzher@inbox.ru>
2015-06-17 12:34:16 +0300

    first example

---
+++ Source/RMQ.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace RMQ.Consumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine("-> Waiting for messages. To exit press CTRL+C");
+                    while (true)
+                    {
+                        BasicDeliverEventArgs ea = consumer.Queue.Dequeue();
+
+                        byte[] body = ea.Body;
+                        string message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("Received -> {0}", message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/RMQ.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+
+
+namespace RMQ.Producer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    Console.WriteLine("-> Waiting for send messages. To exit press CTRL+C");
+
+                    string line;
+                    while ((line = Console.ReadLine()) != null)
+                    {
+                        byte[] body = Encoding.UTF8.GetBytes(line);
+
+                        channel.BasicPublish("", "hello", null, body);
+
+                        Console.WriteLine("Sent -> {0}", line);
+                    }
+                }
+            }
+        }
+    }
+}
