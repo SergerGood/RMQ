8bae5c37068f8057982a470c312a0672d7732986
SergerGood <serzher@inbox.ru>
2015-08-11 16:50:53 +0300

    xunit -theory sample

--- Source/RMQ.Tests/SimpleTests.cs
+++ Source/RMQ.Tests/SimpleTests.cs
@@ -17,6 +17,20 @@ namespace RMQ.Tests
             Assert.Equal(5, Add(2, 2));
         }
 
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
         int Add(int x, int y)
         {
             return x + y;
