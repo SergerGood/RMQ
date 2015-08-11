178927b259d48201882a8546c67af4a5b7b76c68
SergerGood <serzher@inbox.ru>
2015-08-11 17:08:22 +0300

    xunit -SharedContext

---
+++ Source/RMQ.Tests/Sample/SharedContextTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+
+namespace RMQ.Tests.Sample
+{
+    public class SharedContextTests : IDisposable
+    {
+        private readonly List<int> collection;
+
+        public SharedContextTests()
+        {
+            collection = new List<int> { 1, 2, 3 };
+        }
+
+        public void Dispose()
+        {
+            collection.Clear();
+        }
+
+        [Fact]
+        public void CheckCount()
+        {
+            var count = collection.Count;
+            Assert.Equal(3, count);
+        }
+
+        [Fact]
+        public void CheckCountAfterAdd()
+        {
+            collection.AddRange(new [] { 4, 5 });
+            var count = collection.Count;
+
+            Assert.Equal(5, count);
+        }
+    }
+}
