a10413d2b129ed1c09b5c23d360f86b43c885172
SergerGood <serzher@inbox.ru>
2015-08-11 16:50:20 +0300

    xunit fact sample

---
+++ Source/RMQ.Tests/SimpleTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+
+namespace RMQ.Tests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
